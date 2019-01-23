0358903c502fe4ce221ebe2a061835b2b2e2dfd0
edwardr <lopezredd@gmail.com>
2019-01-23 17:53:49 -0400

    Add HomePage styling
    Add movie and shadow background resources, Background video takes full-screen and there's a message at the center of the page.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,24 @@
 import React from 'react';
-
 export default function Home() {
 
     return (
-        <div>
-            Home Route
+        <div className="home">
+            <div className="full-screen-video">
+                <video  id="background-video" loop autoPlay>
+                    <source src={"/video/planetyze.mp4"} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+            </div>
+
+
+            <div className="container-overlay">
+                <div className="center-screen">
+                    <h1> Innovate a new travel style </h1>
+                </div>
+                {/*<h1> Innovate a new travel style </h1>*/}
+
+            </div>
+
         </div>
     )
 
