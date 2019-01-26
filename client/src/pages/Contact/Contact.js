a6f064765cd179b56e335a66f184a240a3993aaf
edwardr <lopezredd@gmail.com>
2019-01-26 18:19:23 -0400

    Update Contact and ContactForm component to render api data
    Handle network error messages. After success a message is shown

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import {Grid} from "react-bootstrap";
+import connect from "react-redux/es/connect/connect";
 
 import './style.css';
 import Head from "../../components/Head";
 import SubHeader from "../../components/SubHeader";
 import ContactForm from './ContactForm';
+import {submitForm} from "../../store/actions/contact";
+import {toast, ToastContainer} from "react-toastify";
 
 function Contact(props) {
+
     function submit(values) {
-        // print the form values to the console
-        console.log(values)
+        const {submitForm} = props;
+        submitForm(values);
+    }
+
+    const {contact} = props;
+
+    if (contact.error) {
+        console.log("toast called");
+        toast.error(contact.error.message, {
+            position: toast.POSITION.TOP_CENTER
+        });
     }
 
     return (
@@ -17,10 +30,23 @@ function Contact(props) {
             <Head title={"Contact"}/>
             <SubHeader title={"Contact"}/>
             <Grid>
-                <ContactForm onSubmit={submit}/>
+                <ContactForm success={contact.success} isSubmitting={contact.isSubmitting} onSubmit={submit}/>
             </Grid>
+            <ToastContainer/>
         </div>
     );
 }
 
-export default Contact;
+function mapStateToProps({contact}) {
+    return {
+        contact,
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        submitForm: (form) => dispatch(submitForm(form)),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Contact);
--- client/src/pages/Contact/ContactForm.js
+++ client/src/pages/Contact/ContactForm.js
@@ -1,29 +1,40 @@
 import React from 'react'
 import {Field, reduxForm} from 'redux-form'
-import {Form, ControlLabel, FormGroup, Button, Glyphicon} from 'react-bootstrap';
+import {Form, ControlLabel, FormGroup, Button, Glyphicon, Alert} from 'react-bootstrap';
 
 import FieldInput from "../../components/FieldInput";
 
 let ContactForm = props => {
-    const {handleSubmit, pristine, submitting} = props;
+    const {handleSubmit, pristine, isSubmitting, success} = props;
+
+    if (success) {
+        return (
+            <div className="text-center">
+                <Alert bsStyle="success">
+                    <span>Your message was sent successfully</span>
+                </Alert>;
+            </div>
+        )
+    }
+
     return (
         <Form className="contact-form" horizontal onSubmit={handleSubmit}>
-            <FormGroup controlId="formControlsName">
+            <FormGroup>
                 <ControlLabel htmlFor="name">Name</ControlLabel>
                 <Field required name="name" component={FieldInput} type="text"/>
             </FormGroup>
 
-            <FormGroup controlId="formControlsEmail">
+            <FormGroup>
                 <ControlLabel htmlFor="email">Email</ControlLabel>
                 <Field required name="email" component={FieldInput} componentClass="input" type="email"/>
             </FormGroup>
 
-            <FormGroup controlId="formControlsMessage">
+            <FormGroup>
                 <ControlLabel htmlFor="message">Message</ControlLabel>
-                <Field name="message" component={FieldInput} componentClass="textarea" rows="8" type="text"/>
+                <Field required name="message" component={FieldInput} componentClass="textarea" rows="8" type="text"/>
             </FormGroup>
 
-            <Button bsStyle="primary" disabled={pristine || submitting} type="submit">
+            <Button bsStyle="primary" disabled={pristine || isSubmitting} type="submit">
                 <Glyphicon glyph="send"/>
                 Submit</Button>
         </Form>
