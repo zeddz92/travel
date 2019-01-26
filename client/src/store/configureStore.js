6192af3f76a7f0ea5d696ec2dcd7a923bb89d8d2
edwardr <lopezredd@gmail.com>
2019-01-25 22:07:06 -0400

    Create axios instance to be used for async actions

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -1,14 +1,15 @@
 import {createStore, compose, applyMiddleware} from 'redux';
 import {persistStore, persistReducer} from 'redux-persist';
+import axios from 'axios';
+
 import thunk from 'redux-thunk';
 import storage from 'redux-persist/lib/storage';
 import reducer from './reducers/index';
-import {api} from '../utils/api';
 
 const persistConfig = {
     key: 'root',
     storage: storage,
-    blacklist: ['error', 'loading', 'form']
+    blacklist: ['error', 'form']
 };
 
 const rootReducer = (state, action) => {
@@ -18,9 +19,13 @@ const rootReducer = (state, action) => {
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:5000',
+});
+
 export default (state = {}) => {
     let store = createStore(persistedReducer, state, composeEnhancers(
-        applyMiddleware(thunk.withExtraArgument(api)),
+        applyMiddleware(thunk.withExtraArgument(axiosInstance)),
     ));
 
     let persistor = persistStore(store);
