0f838ddd7927e91d3b931013c1be9a2a63cd3502
edwardr <lopezredd@gmail.com>
2019-01-30 16:53:44 -0400

    Render services links from store
    Change from staless to pure component to fetch services.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import {withRouter} from 'react-router-dom';
 import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 import i18next from 'i18next';
+import {connect} from "react-redux";
 
+import {fetchServicesIfNeeded} from '../store/actions/service';
 import * as routes from '../utils/routes';
 
 function toggleLanguage() {
     const currentLanguage = i18next.language;
 
-    const callback = (t) => {
+    const callback = () => {
         window.location.reload();
     };
 
@@ -17,56 +19,80 @@ function toggleLanguage() {
     return i18next.changeLanguage(language, callback);
 }
 
-function Header(props) {
-    console.log(props);
-    const {location} = props;
+class Header extends PureComponent {
 
-    //If current route is home header will always be shown, otherwise give normal index for modals and gallery
-    const zIndex = location.pathname === routes.HOME ? 1030 : 0;
+    componentDidMount() {
+        const {fetchServices} = this.props;
 
-    return (
-        <div className="header">
-            <Navbar style={{zIndex: zIndex}} inverse collapseOnSelect>
-                <Navbar.Header>
-                    <Navbar.Brand>
-                        <a href={"/"}>Travelience</a>
-                    </Navbar.Brand>
-                    <Navbar.Toggle/>
-                </Navbar.Header>
+        fetchServices();
+    }
 
-                <Navbar.Collapse>
-                    <Nav pullRight>
-                        <NavDropdown eventKey={1} title={i18next.t('company')} id="basic-nav">
-                            <MenuItem href={routes.ABOUT} eventKey={1.1}>{i18next.t('about')}</MenuItem>
-                            <MenuItem href={routes.STAFF} eventKey={1.2}>{i18next.t('staff')}</MenuItem>
-                            <MenuItem href={routes.JOBS} eventKey={1.3}>{i18next.t('jobs')}</MenuItem>
-                            <MenuItem href={routes.CONTACT} eventKey={1.4}>{i18next.t('contact')}</MenuItem>
-                        </NavDropdown>
+    render() {
+        const {location, services} = this.props;
 
-                        <NavItem eventKey={2} href={routes.JOBS}>
-                            {i18next.t('jobs')}
-                        </NavItem>
+        //If current route is home header will always be shown, otherwise give normal index for modals and gallery
+        const zIndex = location.pathname === routes.HOME ? 1030 : 0;
 
-                        <NavItem eventKey={3} href={routes.SERVICES}>
-                            {i18next.t('services')}
-                        </NavItem>
+        return (
+            <div className="header">
+                <Navbar style={{zIndex: zIndex}} inverse collapseOnSelect>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a href={"/"}>Travelience</a>
+                        </Navbar.Brand>
+                        <Navbar.Toggle/>
+                    </Navbar.Header>
 
-                        <NavItem eventKey={4} href={routes.PRESS}>
-                            {i18next.t('press')}
-                        </NavItem>
+                    <Navbar.Collapse>
+                        <Nav pullRight>
+                            <NavDropdown eventKey={1} title={i18next.t('company')} id="basic-nav">
+                                <MenuItem href={routes.ABOUT} eventKey={1.1}>{i18next.t('about')}</MenuItem>
+                                <MenuItem href={routes.STAFF} eventKey={1.2}>{i18next.t('staff')}</MenuItem>
+                                <MenuItem href={routes.JOBS} eventKey={1.3}>{i18next.t('jobs')}</MenuItem>
+                                <MenuItem href={routes.CONTACT} eventKey={1.4}>{i18next.t('contact')}</MenuItem>
+                            </NavDropdown>
 
-                        <NavItem eventKey={5} href={routes.CONTACT}>
-                            {i18next.t('contact')}
-                        </NavItem>
+                            <NavItem eventKey={2} href={routes.JOBS}>
+                                {i18next.t('jobs')}
+                            </NavItem>
 
-                        <NavItem onClick={toggleLanguage} eventKey={6}>
-                            {i18next.t('language')}
-                        </NavItem>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        </div>
-    );
-};
+                            <NavDropdown eventKey={1} title={i18next.t('services')} id="basic-nav">
+                                {services.items.map((service, index) => (
+                                    <MenuItem key={index} href={`/services/${service.path}`}
+                                              eventKey={`{1.${index + 1}}`}>{service.name}</MenuItem>
+                                ))}
+                            </NavDropdown>
 
-export default withRouter(Header);
+                            <NavItem eventKey={4} href={routes.PRESS}>
+                                {i18next.t('press')}
+                            </NavItem>
+
+                            <NavItem eventKey={5} href={routes.CONTACT}>
+                                {i18next.t('contact')}
+                            </NavItem>
+
+                            <NavItem onClick={toggleLanguage} eventKey={6}>
+                                {i18next.t('language')}
+                            </NavItem>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+
+function mapStateToProps({services}) {
+    return {
+        services,
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        fetchServices: () => dispatch(fetchServicesIfNeeded())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Header));
