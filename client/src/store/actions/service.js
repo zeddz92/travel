1f83d49f4f3b78ef43941db18cf2790dc73b80ca
edwardr <lopezredd@gmail.com>
2019-01-30 16:11:10 -0400

    Create service action creator and update reducer

---
+++ client/src/store/actions/service.js
@@ -0,0 +1,62 @@
+import i18next from 'i18next';
+
+import * as actionTypes from './types';
+import {shouldFetch, isDataStale} from './helpers';
+
+function requestServices() {
+    return {
+        type: actionTypes.REQUEST_SERVICES,
+        error: false
+    }
+}
+
+function receiveServices(res) {
+    return {
+        type: actionTypes.RECEIVE_SERVICES,
+        payload: {
+            data: res.data,
+            lng: i18next.language
+        },
+        error: false
+    }
+}
+
+function errorServices(error) {
+    const response = error.response ? error.response : {};
+    return {
+        type: actionTypes.ERROR_SERVICES,
+        error: {message: error.message, code: response.status}
+    }
+}
+
+function invalidateServices() {
+    return {
+        type: actionTypes.INVALIDATE_SERVICES
+    }
+}
+
+
+
+const fetchServices = () => (dispatch, getState, api) => {
+    dispatch(requestServices());
+
+    return api.get(`/services?lng=${i18next.language}`)
+        .then(
+            response => dispatch(receiveServices(response)),
+            error => dispatch(errorServices(error))
+        );
+};
+
+
+export const fetchServicesIfNeeded = () => (dispatch, getState) => {
+    const services = getState().services;
+    if(isDataStale(services)) {
+        dispatch(invalidateServices())
+    }
+
+    if (shouldFetch(services)) {
+        return dispatch(fetchServices())
+    }
+
+    return Promise.resolve();
+};
--- client/src/store/reducers/services.js
+++ client/src/store/reducers/services.js
@@ -1,7 +1,48 @@
-const defaultState = [];
+import * as actionTypes from "../actions/types";
 
-export function services(state = defaultState, action) {
+const initialState = {
+    isFetching: false,
+    didInvalidate: false,
+    lastUpdated: 0,
+    error: null,
+    items: []
+};
+
+export function services(state = initialState, action) {
     switch (action.type) {
+
+        case actionTypes.INVALIDATE_SERVICES:
+            return {
+                ...state,
+                didInvalidate: true
+            };
+
+        case actionTypes.ERROR_SERVICES:
+            return {
+                ...state,
+                isFetching: false,
+                error: action.error
+            };
+
+        case actionTypes.REQUEST_SERVICES:
+            return {
+                ...state,
+                isFetching: true,
+                didInvalidate: false,
+            };
+
+        case actionTypes.RECEIVE_SERVICES:
+            return {
+                ...state,
+                isFetching: false,
+                didInvalidate: false,
+                items: action.payload.data,
+                lng: action.payload.lng,
+                lastUpdated: Date.now(),
+                error: null
+            };
+
+
         default:
             return state;
     }
