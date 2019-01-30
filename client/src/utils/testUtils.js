f08f823509c40a8ed5453618630eb4f538b8bf01
edwardr <lopezredd@gmail.com>
2019-01-29 21:58:47 -0400

    Create mockStore function for testing connected components
    Returns stores functions like subscribe, dispatch and getState

--- client/src/utils/testUtils.js
+++ client/src/utils/testUtils.js
@@ -3,6 +3,21 @@ import {createStore} from 'redux';
 import reducer from '../store/reducers';
 import {middlewares} from '../store/configureStore';
 
+// store for action creator tests
 export const storeFactory = (initialState = {}) => {
     return createStore(reducer, initialState, middlewares);
+};
+
+// store for components tests
+export const mockStore = (state = {}) => {
+    return  {
+        subscribe: () => {},
+        dispatch: () => {},
+        getState: () => (state)
+    };
+
+};
+
+export const findByTestAttr = (wrapper, val) => {
+    return wrapper.find(`[data-test='${val}']`)
 };
